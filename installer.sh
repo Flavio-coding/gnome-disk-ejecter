5dea7a39ced031eb6d29c5aeebc7b12bffe02621
Flavio-coding <flavio.ranzenigo@gmail.com>
2025-05-22 15:49:07 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@
 # ----------- INTESTAZIONE GRAFICA -----------
 echo -e "\033[1;34m"
 echo "╔══════════════════════════════════════════════════════════════════════╗"
-echo "║   INSTALLAZIONE DELLO SCRIPT DI ESPULSIONE SICURA DEI DISCHI         ║"
+echo "║   INSTALLAZIONE DELLO SCRIPT DI ESPULSIONE SICURA DEI DISCHI    jdjfdjdjdjdj     ║"
 echo "╚══════════════════════════════════════════════════════════════════════╝"
 echo -e "\033[0m"
 
